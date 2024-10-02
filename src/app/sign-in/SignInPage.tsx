f0ae69490c1c3a360e81ee610109f68182870f95
Diego Aquino <diegocruzdeaquino@gmail.com>
2024-10-02 11:07:50 -0300

    feat: configure token revalidation

--- src/app/sign-in/SignInPage.tsx
+++ src/app/sign-in/SignInPage.tsx
@@ -3,7 +3,7 @@ import SignInForm from './SignInForm';
 function SignInPage() {
   return (
     <div className="flex flex-1 items-center justify-center">
-      <main className="mx-auto flex w-fit flex-col items-center space-y-4 rounded-lg bg-white p-6">
+      <main className="mx-auto flex w-fit flex-col items-center space-y-4 rounded-lg bg-white p-6 shadow-lg">
         <h1 className="text-3xl font-medium">Blink</h1>
         <SignInForm />
       </main>
--- src/clients/http.ts
+++ src/clients/http.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 import environment from '@/config/environment';
 
@@ -8,5 +8,80 @@ const http = {
     withCredentials: true,
   }),
 };
+
+function isLoginRequest(request: unknown) {
+  return (
+    typeof request === 'object' &&
+    request !== null &&
+    'responseURL' in request &&
+    typeof request.responseURL === 'string' &&
+    request.responseURL.endsWith('/auth/login')
+  );
+}
+
+function isAuthRefreshRequest(request: unknown) {
+  return (
+    typeof request === 'object' &&
+    request !== null &&
+    'responseURL' in request &&
+    typeof request.responseURL === 'string' &&
+    request.responseURL.endsWith('/auth/refresh')
+  );
+}
+
+export function isUnauthenticatedResponse(response: AxiosResponse) {
+  const responseData = response.data as unknown;
+
+  const hasUnauthenticatedMessage =
+    typeof responseData === 'object' &&
+    responseData !== null &&
+    'message' in responseData &&
+    responseData.message === 'Authentication is required to access this resource.';
+
+  const hasInvalidCredentialsMessage =
+    !isLoginRequest(response.request) &&
+    typeof responseData === 'object' &&
+    responseData !== null &&
+    'message' in responseData &&
+    responseData.message === 'Authentication credentials are not valid.';
+
+  return response.status === 401 && (hasUnauthenticatedMessage || hasInvalidCredentialsMessage);
+}
+
+function redirectToSignIn() {
+  window.location.pathname = '/sign-in';
+}
+
+http.backend.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (!(error instanceof AxiosError) || !error.response) {
+      return Promise.reject(error);
+    }
+
+    const isUnauthenticated = isUnauthenticatedResponse(error.response);
+    const isRefreshRequest = isAuthRefreshRequest(error.request);
+
+    const isRefreshTokenExpiredError = isRefreshRequest && error.response.status >= 400;
+    const isAccessTokenExpiredError = isUnauthenticated && !isRefreshTokenExpiredError;
+
+    if (isAccessTokenExpiredError) {
+      try {
+        await http.backend.post('/auth/refresh');
+      } catch (refreshError) {
+        console.error(refreshError);
+        return Promise.reject(error);
+      }
+
+      return axios(error.config ?? {});
+    }
+
+    if (isRefreshTokenExpiredError) {
+      redirectToSignIn();
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default http;
